e83ae4cb5b2699f6641431032e8e38b8f32d08ed
Ladi <ladismrkolj@gmail.com>
2023-09-04 13:12:27 +0200

    cleanup and more some multiple test code

--- src/revonto/reverse_lookup.py
+++ src/revonto/reverse_lookup.py
@@ -1,24 +1,9 @@
 class ReverseLookupRecord(object):
     """Represents one result (from a single product) in the ReverseLookupStudy"""
-    def __init__(self, name, **kwargs):
-        # Methods seen in current enrichment result
-        # pylint: disable=invalid-name
-        self.name = name
-        self.method_flds = []
-        self.kws = kwargs
-        # Ex: ratio_in_pop ratio_in_study study_items p_uncorrected pop_items
-        for key, val in kwargs.items():
-            setattr(self, key, val)
-        _stucnt = kwargs.get("ratio_in_study", (0, 0))
-        self.study_count = _stucnt[0]
-        self.study_n = _stucnt[1]
-        _popcnt = kwargs.get("ratio_in_pop", (0, 0))
-        self.pop_count = _popcnt[0]
-        self.pop_n = _popcnt[1]
-        self.enrichment = self._init_enrichment()
+    def __init__(self):
+        pass
 
-    def set_corrected_pval(self, nt_method, pvalue):
-        """Add object attribute based on method name."""
-        self.method_flds.append(nt_method)
-        fieldname = "".join(["p_", nt_method.fieldname])
-        setattr(self, fieldname, pvalue)
+class GOReverseLookupStudy():
+    """Runs pvalue test, as well as multiple corrections"""
+    def __init__(self):
+        pass
