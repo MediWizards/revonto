622dd4273e3a1b56959996f7b1b7267aaa957627
ladismrkolj <ladismrkolj@gmail.com>
2023-09-22 17:31:44 +0200

    Add ortholog and convert_id (#10)
    * from object oriented to functional for some
    
    * fix: typing
    
    * feat: add gOrth orthologand base ortholog class
    
    * intermittent ortholog commit
    
    * feat: add ortholog and convert_id

--- src/revonto/pvalcalc.py
+++ src/revonto/pvalcalc.py
@@ -1,80 +1,45 @@
 """Options for calculating uncorrected p-values."""
+from scipy import stats
 
 
-class PvalueCalcBase:
-    """Base class for calculating p-value"""
+def fisherscipystats(study_count, study_n, pop_count, pop_n) -> float:
+    """_summary_
 
-    def __init__(self, pval_fnc) -> None:
-        self.pval_fnc = pval_fnc
+    Args:
+        study_count (_type_): _description_
+        study_n (_type_): _description_
+        pop_count (_type_): _description_
+        pop_n (_type_): _description_
 
-    def calc_pvalue(
-        self, study_count: int, study_n: int, pop_count: int, pop_n: int
-    ) -> float:
-        """pvalues are calculated in derived classes"""
-        raise NotImplementedError("Must be called from derived classes")
+    Returns:
+        _type_: _description_
+    """
+    avar = study_count
+    bvar = study_n - study_count
+    cvar = pop_count - study_count
+    dvar = pop_n - pop_count - bvar
 
+    _, pval = stats.fisher_exact([[avar, bvar], [cvar, dvar]])
 
-class FisherScipyStats(PvalueCalcBase):
-    """Fisher exact test"""
-
-    def __init__(self) -> None:
-        from scipy import stats
-
-        super().__init__(stats.fisher_exact)
-
-    def calc_pvalue(self, study_count, study_n, pop_count, pop_n):
-        """Calculate uncorrected pvalues."""
-        #               YES       NO
-        # study_genes    a scnt   b    | a+b = study_n
-        # not s_genes    c        d    |  c+d
-        #             --------   -----
-        #   pop_genes  a+c pcnt   b+d  a+b+c+d = pop_n
-
-        avar = study_count
-        bvar = study_n - study_count
-        cvar = pop_count - study_count
-        dvar = pop_n - pop_count - bvar
-        if cvar < 0:
-            raise ValueError(
-                f"STUDY={avar}/{bvar} POP={cvar}/{dvar} scnt({study_count}) stot({study_n}) pcnt({pop_count}) ptot({pop_n})"
-            )
-        _, p_uncorrected = self.pval_fnc([[avar, bvar], [cvar, dvar]])
-        return p_uncorrected
+    return pval
 
 
-class BinomialScipyStats(PvalueCalcBase):
-    """Binomial test"""
+def binomialscipystats(study_count, study_n, pop_count, pop_n) -> float:
+    k = study_count
+    n = study_n
+    p = pop_count / pop_n
 
-    def __init__(self) -> None:
-        from scipy import stats
+    pval = stats.binomtest(k, n, p).pvalue
 
-        super().__init__(stats.binomtest)
-
-    def calc_pvalue(self, study_count, study_n, pop_count, pop_n):
-        """Calculate uncorrected pvalues."""
-        k = study_count
-        n = study_n
-        p = pop_count / pop_n
-        if pop_n == 0:
-            raise ZeroDivisionError("pop_n is equal to zero")
-        p_uncorrected = self.pval_fnc(k, n, p).pvalue
-        return p_uncorrected
+    return pval
 
 
-class PValueFactory:
-    """Factory for choosing an algorithm"""
+def pvalue_calculate(study_count, study_n, pop_count, pop_n, method) -> float:
+    if method == "fisher_scipy_stats":
+        pval = fisherscipystats(study_count, study_n, pop_count, pop_n)
+    elif method == "binomial_scipy_stats":
+        pval = binomialscipystats(study_count, study_n, pop_count, pop_n)
+    else:
+        raise ValueError(f"{method} is not an available method for calculating pvalue")
 
-    options = {
-        "fisher_scipy_stats": FisherScipyStats,
-        "binomial_scipy_stats": BinomialScipyStats,
-    }
-
-    def __init__(self, pvalcalc="fisher_scipy_stats") -> None:
-        if pvalcalc not in self.options:
-            raise ValueError(f"pvalcalc must be one of {self.options.keys()}")
-        self.pval_fnc_name = pvalcalc
-        self.pval_obj: PvalueCalcBase = self._init_pval_obj()
-
-    def _init_pval_obj(self):
-        """Returns pvalue object based on the input"""
-        return self.options[self.pval_fnc_name]()
+    return pval
