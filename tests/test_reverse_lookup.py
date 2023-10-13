ad8c08f26d00bde8bd395b15c88c64d0563df8aa
Ladi <ladismrkolj@gmail.com>
2023-10-13 23:23:54 +0200

    fix: test_reverse_lookup wrong p

--- tests/test_reverse_lookup.py
+++ tests/test_reverse_lookup.py
@@ -11,7 +11,7 @@ def test_reverse_lookup_study(annotations_test, godag_test):
     results = study.run_study(studyset)
 
     assert results[0].object_id == "UniProtKB:A0A024RBG1"
-    assert pytest.approx(results[0].pvals["uncorrected"]) == 0.14285714285714288
+    assert pytest.approx(results[0].pvals["uncorrected"]) == 0.20000000000000004
     assert (
-        pytest.approx(results[0].pvals["bonferroni"]) == 0.14285714285714288
+        pytest.approx(results[0].pvals["bonferroni"]) == 0.20000000000000004
     )  # only one test was done
